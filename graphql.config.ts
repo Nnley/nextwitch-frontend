1508a5ab66821adc510fc1162cc5047f60fa7d01
Nnley <kragelek86@gmail.com>
2025-02-22 21:33:18 +0300

    Added GraphQL query and codegen scripts

--- graphql.config.ts
+++ graphql.config.ts
@@ -1,7 +1,8 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
+import 'dotenv/config'
 
 const config: CodegenConfig = {
-  schema: process.env.NEXT_PUBLIC_API_URL,
+  schema: process.env.NEXT_PUBLIC_SERVER_URL,
   documents: ['./src/graphql/**/*.graphql'],
   generates: {
     './src/graphql/generated/output.ts': {
